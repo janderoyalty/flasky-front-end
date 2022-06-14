474fb18e9f9a147bcb6f10d43f0fc67dc67c10f8
Jande Royalty <janderoyalty@gmail.com>
2022-06-14 13:05:04 -0500

    Added Dog & DogList components

---
+++ src/components/Dog.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Dog = () => {
+  return (
+    <li>
+      <h3>A Soon to be Name Dog</h3>
+      <span>Chip #: 123</span>
+      <button>add Chip</button>
+    </li>
+  );
+};
